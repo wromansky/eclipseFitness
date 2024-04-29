f2efab229e8c0bf91aec7c895d4b7160a99cc708
Billy Romansky <wromansky@protonmail.com>
2024-04-28 22:41:34 -0400

    added text content

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,20 +1,11 @@
 module.exports = {
-    author: "Your Name",
-    siteName: "11ty Fylgja Blog",
-    siteDescription: "Your cool new blog, built with Eleventy and Fylgja CSS.",
+    author: "Billy Romansky",
+    siteName: "Eclipse Fitness",
+    siteDescription: "Official website for Eclipse Fitness",
     themeColor: "#6001ff",
-    email: "info@site.dev",
-    phone: "+47 988 988 988",
-    address: {
-        name: "Fylgja 11ty",
-        street: "the web 11",
-        postcode: "00-000",
-        city: "Zwollywood",
-        region: "Overijsel",
-        country: "Netherlands",
-    },
+    email: "austinyannone@gmail.com",
+    phone: "443-875-7837",
     socials: {
-        twitter: "https://twitter.com/FylgjaDev/",
-        github: "https://github.com/fylgja/",
+        instagram: "https://instagram.com"
     },
 };
