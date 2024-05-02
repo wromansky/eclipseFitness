7ad88e0dabb8f9f71708e93a0d33120e0c21ff70
Billy Romansky <wromansky@protonmail.com>
2024-05-01 22:43:37 -0400

    Updated theme

--- src/_data/themes.js
+++ src/_data/themes.js
@@ -4,18 +4,27 @@ const colorsHsl = require("@fylgja/colors/hsl");
 module.exports = {
     default: {
         name: "Light",
-        theme: "#1565c1",
-        colors: false,
+        theme: "#470652",
+        colors: {
+            "--color-bg": "#fff",
+            "--color-text": "#111",
+            "--color-text-alt": "hsl(0 0% 100% / 82%)",
+            "--color-text-muted": "hsl(0 0% 100% / 56%)",
+            "--code-bg": "hsl(291 86% 17%)",
+            "--code-color": "#fff",
+            "--shadow-weight": "25%",
+            "--shadow-color": "220 40% 2%",
+        },
     },
     dark: {
         name: "Dark",
-        theme: "#1565c1",
+        theme: "#470652",
         colors: {
             "--color-bg": "#111",
             "--color-text": "#fff",
             "--color-text-alt": "hsl(0 0% 100% / 82%)",
             "--color-text-muted": "hsl(0 0% 100% / 56%)",
-            "--code-bg": "hsl(0 0% 20%)",
+            "--code-bg": "hsl(291 86% 17%)",
             "--code-color": "#fff",
             "--shadow-weight": "25%",
             "--shadow-color": "220 40% 2%",
